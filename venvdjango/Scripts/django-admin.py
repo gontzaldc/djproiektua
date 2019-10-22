f8bbab3ef52dd86f9d435228fe90f773fd009871
DW3-2-12 <DW3-2-12@uni.lan>
2019-10-22 13:42:40 +0200

    proiektua hasi

---
+++ venvdjango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dw3-2-12.uni\documents\django\djproiektua\venvdjango\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
